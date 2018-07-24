011a97f330e56e1fdab65f285c233359ec35a38d
skyz-da-limit13 <jayedot13@gmail.com>
2018-07-23 17:31:58 -0700

    style changes made to all files

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #File: guessinggame.sh
 
-actual=$(ls | wc -w)
-flag=0
+num_of_files=$(ls | wc -w)
+done_flag=0
 	
-function decision {
-	diff=$(expr $actual - $guess)
+function  game_results {
+	diff=$(expr $1 - $2)
 
 	if [[ $diff -eq 0 ]] 
 	then
-		echo "!!!CONGRATULATIONS!!! YOU GUESSED IT BUDDDY!!!"
+		echo "!!!CONGRATULATIONS!!! YOU GUESSED IT!!!"
 		echo ""
-		let flag=1
+		let done_flag=1
 	elif [[ $diff -gt 0 ]]
 	then
 		echo "Sorry, but thats too low"
@@ -21,10 +21,10 @@ function decision {
 	fi
 }
 
-while [[ $flag -eq 0 ]]
+while [[ $done_flag -eq 0 ]]
 do
 	echo ""
-	echo "Guess how many files are in the current directory? "
-	read guess
-	decision $actual $guess
+	echo "How many files do you think are in this current directory? "
+	read usr_guess
+	game_results $num_of_files $usr_guess
 done
--- makefile
+++ makefile
@@ -5,7 +5,7 @@ README.md:
 	echo ""
 	echo "Make created on: $$(date)" >> README.md
 	echo "" >> README.md
-	echo "Number of lines in guessinggame.sh: $$(wc -l guessinggame.sh)" >> README.md
+	echo "Number of lines of code in guessinggame.sh: $$(wc -l guessinggame.sh)" >> README.md
 
 clean:
 	rm README.md
